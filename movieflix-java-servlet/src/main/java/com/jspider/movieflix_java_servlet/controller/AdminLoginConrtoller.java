7ba3d0afba292b36ad1f8df09f5458146f1a0aaa
Sachin007060 <sachinbaskandi20@gmail.com>
2024-05-08 00:07:00 +0530

    add adminlogincontroller

--- movieflix-java-servlet/src/main/java/com/jspider/movieflix_java_servlet/controller/AdminLoginConrtoller.java
+++ movieflix-java-servlet/src/main/java/com/jspider/movieflix_java_servlet/controller/AdminLoginConrtoller.java
@@ -1,25 +1,39 @@
 package com.jspider.movieflix_java_servlet.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.jspider.movieflix_java_servlet.dao.AdminDao;
 import com.jspider.movieflix_java_servlet.dto.Admin;
 
-public class AdminLoginConrtoller {
-	public static void main(String[] args) {
-		AdminDao dao = new AdminDao();
+@WebServlet("/adminLogin")
+public class AdminLoginConrtoller extends HttpServlet {	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String email = "sachin@123gmail.com";
-		String password = "sachin@123";
+		AdminDao adminDao = new AdminDao();
 		
-		Admin admin =dao.fetchAdminByEmailForLogin(email);
+		String email = req.getParameter("adminEmail");
+		String password = req.getParameter("adminPassword");
 		
+		Admin admin = adminDao.fetchAdminByEmailForLogin(email);
+				
 		if(admin!=null) {
 			if(admin.getAdminPassword().equals(password)) {
-				System.out.println("Successful Login");
+				req.getRequestDispatcher("admin-home.jsp").forward(req, resp);
 			}else {
-				System.out.println("Password is Incorrect");
+				req.setAttribute("adminMsg", "Password is invalid");
+				req.getRequestDispatcher("admin-login.jsp").forward(req, resp);
 			}
 		}else {
-			System.out.println("Email is Incorrect");
+			req.setAttribute("adminMsg", "Email is invalid");
+			req.getRequestDispatcher("admin-login.jsp").forward(req, resp);
 		}
 	}
 }
