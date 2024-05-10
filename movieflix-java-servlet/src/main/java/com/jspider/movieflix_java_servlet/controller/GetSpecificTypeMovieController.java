646d4a7c53a4dceee77d1c0db8c06c5704074987
Sachin007060 <sachinbaskandi20@gmail.com>
2024-05-10 08:48:51 +0530

    create a file to get the specific type of movie to search by the user

---
+++ movieflix-java-servlet/src/main/java/com/jspider/movieflix_java_servlet/controller/GetSpecificTypeMovieController.java
@@ -0,0 +1,19 @@
+package com.jspider.movieflix_java_servlet.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/getSpecificTypeMovie")
+public class GetSpecificTypeMovieController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
